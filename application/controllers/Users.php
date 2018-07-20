5f80db57fdf732229dc4f614171b8ebf25024662
Joelbabay <babayjoel7@gmail.com>
2018-07-20 15:25:07 +0200

    Registration Login

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -36,6 +36,29 @@ class Users extends CI_Controller {
             $data['error'] = $this->session->userdata('error');
             $this->session->unset_userdata('error');
          }
+
+         if($this->input->post('loginSubmit')) {
+             $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+             $this->form_validation->set_rules('password', 'password', 'required');
+             if ($this->form_validation->run() == true) {
+                $connect['returnType'] = 'single';
+                $connect['condition'] = array(
+                    'email'    =>$this->input->post('email'),
+                    'password' =>$this->input->post('password'),
+                    'status'   => '1'
+                );
+                $check = $this->user->getRows($connect);
+                if ($check) {
+                    $this->session->set_userdata('isUserLoggedIn', TRUE);
+                    $this->session->set_userdata('userId', $check['id']);
+                    redirect('users/account/');
+                } else {
+                    $data['error'] = 'Worng password or email, please try again';
+                }
+             }     
+         }
+        //load view
+        $this->load->view('users/login', $data);
      }
 
 }
